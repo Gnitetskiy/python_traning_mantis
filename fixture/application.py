d7fdd7f862d3d4661a533556e0e61534e7591298
Gnitetskiy Alexey <agnitetskiy@nobilis.team>
2025-08-13 12:40:17 +0300

    добавление фикстур и теста для првоерки регистрации

--- fixture/application.py
+++ fixture/application.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
+
+from fixture.mail import MailHelper
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
+from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+
 
 
 
 class Application:
-    def __init__(self, browser, base_url ):
+    def __init__(self, browser, config ):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -15,8 +20,12 @@ class Application:
         else:
             raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
-        self.base_url= base_url
+        self.james = JamesHelper(self)
+        self.config=config
+        self.base_url= config['web']['baseUrl']
         self.project = ProjectHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
